fd54c13dc32e20eb021ab6842a8b015467ee309e
abhikavi <75289701+abhikavi@users.noreply.github.com>
2021-10-13 17:35:32 +0530

    added workspace details

--- main.tf
+++ main.tf
@@ -5,10 +5,17 @@ terraform {
       #version = "~> 3.21" # Optional but recommended in production
     }
   }
-}
+
 
 # Provider Block
 provider "aws" {
   profile = "default" 
   region  = "us-east-1"
+}
+backend "remote" {
+  organization = "Mouritech"
+  workspaces {
+    name = "github-actions-demo"
+  }
+}
 }
